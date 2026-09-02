8ea322ae72194a82f4a3c8dc8cebdd78a0d2f97c
Ansh Rai <anshrai446@gmail.com>
2026-09-02 22:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (87.60%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // since if all are even then copy it same for odd
+        //if at leat one is even copy the odd one and -1 from even to make it odd
+        return true;
+    }
+}
