fc1506efba48e59545c52f0cf169e74e1e79979c
Ansh Rai <anshrai446@gmail.com>
2026-09-26 10:28:06 +0530

    Time: 1 ms (70.46%) | Memory: 42.9 MB (92.09%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,27 +1,28 @@
 class Solution {
-    static int lowerbound(int [] days , int target){
-        int left = 0;
-        int right = days.length;
-        while(left<right){
-            int mid = left+(right-left)/2;
-            if(days[mid] >= target) right=mid;
-            else left=mid+1;
+    static int lowerBound (int days [] , int target){
+        int i=0;
+        int j = days.length;
+        while(i<j){
+            int mid = i+(j-i) / 2;
+            if(days[mid]>=target) j = mid;
+            else i = mid+1;
         }
-        return left;
+        return i;
     }
-    static int solve(int[] days  , int [] costs , int i ,  int dp[]){
-        if(i>=days.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int cost1 = costs[0]+solve(days,costs,i+1,dp);
-       int next7=lowerbound(days,days[i]+7);
-        int cost2 = costs[1]  + solve(days,costs,next7,dp);
-        int next30 =  lowerbound(days,days[i]+30);
-        int cost3 = costs[2] + solve(days,costs,next30,dp);
-        return dp[i] = Math.min(cost1,Math.min(cost2,cost3));
+    static int  solve( int [] days , int [] cost , int i , Integer [] dp){
+        if(i>=days.length){
+            return 0;
+        }
+        if(dp[i]!=null) return dp[i];
+        int cost1 = cost[0]  + solve(days , cost , i+1 , dp);
+        int next7 = lowerBound(days,days[i]+7);
+        int cost7 = cost[1] + solve(days,cost,next7 , dp);
+        int next30 = lowerBound(days,days[i]+30);
+        int cost30 = cost[2] + solve(days,cost,next30 , dp);
+        return dp[i] = Math.min(cost1,Math.min(cost7,cost30));
     }
     public int mincostTickets(int[] days, int[] costs) {
-       int dp[] = new int[days.length+1];
-        java.util.Arrays.fill(dp,-1);
-        return solve(days,costs,0,dp);
+        Integer [] dp = new Integer[days.length+1];
+        return solve(days , costs , 0 , dp);
     }
 }
