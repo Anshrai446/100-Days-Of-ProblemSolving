2e1896bcdb0630900d100a6b6aa1ad37e5b4d58e
Ansh Rai <anshrai446@gmail.com>
2026-09-26 09:53:21 +0530

    Time: 12 ms (24.03%) | Memory: 46.5 MB (78.72%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -28,39 +28,62 @@ class Solution {
 
     //     return dp[index] = Math.max(include, exclude);
     // }
-    static int solve(List<Integer> ip , int index , Map<Integer,Integer> map , Integer [] dp){
-        if(index>=ip.size()) return 0;
-        int next = index+1;
-        if(dp[index]!=null) return dp[index];
-        if(next<ip.size() && ip.get(next)==ip.get(index)+1){
+    // static int solve(List<Integer> ip , int index , Map<Integer,Integer> map , Integer [] dp){
+    //     if(index>=ip.size()) return 0;
+    //     int next = index+1;
+    //     if(dp[index]!=null) return dp[index];
+    //     if(next<ip.size() && ip.get(next)==ip.get(index)+1){
+    //         next++;
+    //     }
+    //     int include = ip.get(index)*map.get(ip.get(index)) + solve(ip,next,map,dp);
+    //     int exclude  = solve(ip,index+1,map,dp);
+    //      return dp[index] = Math.max(include,exclude);
+
+    // }
+    // public int deleteAndEarn(int[] nums) {
+
+    //     // Map<Integer, Integer> map = new HashMap<>();
+
+    //     // for (int num : nums) {
+    //     //     map.put(num, map.getOrDefault(num, 0) + 1);
+    //     // }
+
+    //     // List<Integer> ip = new ArrayList<>(map.keySet());
+    //     // Collections.sort(ip);
+
+    //     // Integer[] dp = new Integer[ip.size()];
+
+    //     // return solve(ip, 0, map, dp);
+    //     Map<Integer,Integer> map = new HashMap<>();
+    //     for(int i : nums){
+    //         map.put(i,map.getOrDefault(i,0)+1);
+    //     }
+    //     List<Integer> ip = new ArrayList<>(map.keySet());
+    //     Collections.sort(ip);
+    //     Integer [] dp = new Integer[ip.size()+1];
+    //     return solve(ip,0,map,dp);
+    // }
+    static int solve(List<Integer> ip , int i , Map<Integer, Integer> map , Integer [] dp){
+        if(i>=ip.size()){
+            return 0;
+        }
+        int next =i+1;
+        if(next < ip.size() && ip.get(next) == ip.get(i)+1){
             next++;
         }
-        int include = ip.get(index)*map.get(ip.get(index)) + solve(ip,next,map,dp);
-        int exclude  = solve(ip,index+1,map,dp);
-         return dp[index] = Math.max(include,exclude);
-
+        if(dp[i]!=null) return dp[i];
+        int include = ip.get(i) * map.get(ip.get(i)) + solve(ip,next,map,dp);
+        int exclude = solve(ip,i+1,map,dp);
+        return dp[i] = Math.max(include,exclude); 
     }
     public int deleteAndEarn(int[] nums) {
-
-        // Map<Integer, Integer> map = new HashMap<>();
-
-        // for (int num : nums) {
-        //     map.put(num, map.getOrDefault(num, 0) + 1);
-        // }
-
-        // List<Integer> ip = new ArrayList<>(map.keySet());
-        // Collections.sort(ip);
-
-        // Integer[] dp = new Integer[ip.size()];
-
-        // return solve(ip, 0, map, dp);
         Map<Integer,Integer> map = new HashMap<>();
-        for(int i : nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        for(int num  : nums){
+            map.put(num , map.getOrDefault(num,0)+1);
         }
-        List<Integer> ip = new ArrayList<>(map.keySet());
+         List<Integer> ip = new ArrayList<>(map.keySet());
         Collections.sort(ip);
-        Integer [] dp = new Integer[ip.size()+1];
-        return solve(ip,0,map,dp);
+        Integer [] dp = new Integer [nums.length];
+       return solve(ip,0,map,dp);
     }
 }
